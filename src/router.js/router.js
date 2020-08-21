eeeafe00aa0f2efa20d3a28eb67f8085e9f680d1
Dulce Mercado <soydulceangelina@gmail.com>
2020-08-21 15:05:31 -0500

    maquetado html

--- src/router.js/router.js
+++ src/router.js/router.js
@@ -1,5 +1,6 @@
 import home from '../pages/home';
 import hola from '../pages/hola';
+import login from '../pages/login';
 
 const router = (route) => {
   switch (route) {
@@ -7,6 +8,8 @@ const router = (route) => {
       return home();
     case '#/hola':
       return hola();
+    case '#/login':
+      return login();
     default:
       return error404();
   }
